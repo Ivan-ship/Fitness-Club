0fd072808a23481d4157be1e596c66f2fd3c183e
Ivan-ship <vanyfug3@gmail.com>
2026-05-30 13:23:13 +0300

    Added delete, insert, update

--- src/routers.py
+++ src/routers.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 from sqlalchemy import select
 from config.database.db_helper import db_helper
-from src.schemas.client_schema import ClientRead
+from src.schemas.client_schema import ClientRead, UpdateClients, ClientCreate
 from src.schemas.clients import Clients
+from sqlalchemy import update, delete
 
 def get_router():
     router = APIRouter()
@@ -20,4 +21,42 @@ def get_router():
 
             clients = result.scalars().all()
             return clients
+
+    #Upadte
+    @router.put("/clients/{client_id}")
+    async def update_client(client_id: int, client: UpdateClients):
+        async with db_helper.session_factory() as session:
+            smt = (
+                update(Clients)
+                .where(Clients.client_id == client_id)
+                .values(
+                    last_name = client.last_name,
+                    first_name = client.first_name,
+                    phone = client.phone
+                    )
+            )
+            await session.execute(smt)
+            await session.commit()
+            return {"message": "Update client"}
+        
+    @router.post("/clients", response_model=ClientRead)
+    async def create_client(client: ClientCreate):
+        async with db_helper.session_factory() as session:
+            new_client = Clients(**client.model_dump())
+            session.add(new_client)
+            await session.commit()
+            await session.refresh(new_client)
+            return new_client
+    
+    #Delete
+    @router.delete("/clients/{client_id}")
+    async def delete_client(client_id: int):
+        async with db_helper.session_factory() as session:
+            smt = (
+                delete(Clients)
+                .where(Clients.client_id == client_id)
+            )
+            await session.execute(smt)
+            await session.commit()
+            return{"message": "Delete Client"}
     return router
--- src/schemas/client_schema.py
+++ src/schemas/client_schema.py
@@ -11,3 +11,16 @@ class ClientRead(BaseModel):
     model_config = {
         "from_attributes": True
     }
+
+#Обновление
+class UpdateClients(BaseModel):
+    last_name: str
+    first_name: str
+    phone: str
+
+class ClientCreate(BaseModel):
+    last_name: str
+    first_name: str
+    phone: str
+    birthday: date
+    registration_day: date
